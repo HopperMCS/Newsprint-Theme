c773304709cdfe3d2154ec04c790d5250de0cd39
travis <travis@browser.html>
2017-06-05 10:11:31 -0400

    COMPLETE SEXY OVERHAUL ONCE AGAIN

--- header.php
+++ header.php
@@ -1,29 +1,38 @@
 <!DOCTYPE html>
 <head>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="http://www.gagesblog.com/bower_components/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="http://www.gagesblog.com/bower_components/tether/dist/css/tether.min.css">
-  <link rel="stylesheet" href="http://www.gagesblog.com/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <script src="http://www.gagesblog.com/bower_components/jquery/dist/jquery.min.js"></script>
-  <script src="http://www.gagesblog.com/bower_components/tether/dist/js/tether.min.js"> </script>
-  <script src="http://www.gagesblog.com/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-  <link href="http://www.gagesblog.com/css/fonts/asar/asar.css" rel="stylesheet">
-  <link href="http://www.gagesblog.com/css/fonts/bitter/bitter.css" rel="stylesheet">
+  <link rel="stylesheet" href="wp-content/themes/newsprint/style.css">
+  <link rel="stylesheet" href="wp-content/themes/newsprint/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="wp-content/themes/newsprint/bower_components/tether/dist/css/tether.min.css">
+  <link rel="stylesheet" href="wp-content/themes/newsprint/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <script src="wp-content/themes/newsprint/bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="wp-content/themes/newsprint/bower_components/tether/dist/js/tether.min.js"> </script>
+  <script src="wp-content/themes/newsprint/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <link href="wp-content/themes/newsprint/css/fonts/asar/asar.css" rel="stylesheet">
+  <link href="wp-content/themes/newsprint/css/fonts/bitter/bitter.css" rel="stylesheet">
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
 </head>
 <html>
   <body>
     <div class="container">
       <hr class="thin">
       <div class="row header">
-        <h1 class="title col-md-6 header text"><img width="45px" height="45px" src="wp-content/themes/newsprint-theme/images/header.png">M. Gage Morgan's Blog</h1>&nbsp;<strong class="tagline"><?php bloginfo( $show = 'description' ); ?></strong>
-        <div class="col-md-2 header push-md-4"><i class="fa fa-facebook-square fa-3x" aria-hidden="true">&nbsp;         </i><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></div>
+        <h1 class="title col-md-8 header text">
+	<img width="45px" height="45px" src="wp-content/themes/newsprint/images/header.png">
+	G-FED
+	<a class="github-button" href="https://github.com/MGageMorgan/Newsprint-Theme/archive/master.zip" data-size="large" aria-label="Download MGageMorgan/Newsprint-Theme on GitHub">Download The "Newsprint" Theme</a>
+	<a class="github-button" href="https://github.com/MGageMorgan" data-size="large" data-show-count="true" aria-label="Follow @MGageMorgan on GitHub">Follow @MGageMorgan</a>&nbsp;
+	</h1>&nbsp;
+        <div class="col-md-3 header push-md-1">	
+	<strong class="tagline"><?php bloginfo( $show = 'description' ); ?></strong>
+	</div>
       </div>
       <div class="row tabs">
         <div class="col-md-12">
           <hr class="head">
           <ul class="nav nav-tabs">
          <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#">
-    <strong>Your local time is <script src="wp-content/themes/newsprint-theme/js/localtime.js"></script></strong></a></li>
+    <strong>Your local time is <script src="wp-content/themes/newsprint/js/localtime.js"></script></strong></a></li>
 <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#"><strong>
 <script>
 $.get("http://ipinfo.io", function (response) {
--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="wp-content/themes/newsprint-theme/style.css">
-  <link rel="stylesheet" href="wp-content/themes/newsprint-theme/bower_components/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="wp-content/themes/newsprint-theme/bower_components/tether/dist/css/tether.min.css">
-  <link rel="stylesheet" href="wp-content/themes/newsprint-theme/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <script src="wp-content/themes/newsprint-theme/bower_components/jquery/dist/jquery.min.js"></script>
-  <script src="wp-content/themes/newsprint-theme/bower_components/tether/dist/js/tether.min.js"> </script>
-  <script src="wp-content/themes/newsprint-theme/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="wp-content/themes/newsprint/style.css">
+  <link rel="stylesheet" href="wp-content/themes/newsprint/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="wp-content/themes/newsprint/bower_components/tether/dist/css/tether.min.css">
+  <link rel="stylesheet" href="wp-content/themes/newsprint/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <script src="wp-content/themes/newsprint/bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="wp-content/themes/newsprint/bower_components/tether/dist/js/tether.min.js"> </script>
+  <script src="wp-content/themes/newsprint/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
   <!-- <link href="wp-content/themes/newsprint-theme/css/fonts/asar/asar.css" rel="stylesheet"> -->
 
 <link href="https://fonts.googleapis.com/css?family=Brawler" rel="stylesheet">
 
-  <link href="wp-content/themes/newsprint-theme/css/fonts/bitter/bitter.css" rel="stylesheet">
+  <link href="wp-content/themes/newsprint/css/fonts/bitter/bitter.css" rel="stylesheet">
 </head>
 <?php get_header(); ?>
     <div class="container">
@@ -34,31 +34,17 @@
 
 <?php if ( have_posts() ) : ?>
 <?php while ( have_posts() ) : the_post(); ?>
-<style>
-
-#featured {
-   background: gray; /* For browsers that do not support gradients */
-   background: -webkit-linear-gradient(white, gray); /* For Safari 5.1 to 6.0 */
-   background: -o-linear-gradient(white, gray); /* For Opera 11.1 to 12.0 */
-   background: -moz-linear-gradient(white, gray); /* For Firefox 3.6 to 15 */
-   background: linear-gradient(white, gray); /* Standard syntax (must be last) */
-   margin-top: 15px;
-   padding: 25px;
-}	
-
-</style>
                         <div class="col-sm-6">
+			<hr class="thin">
 	   <?php
              if ( has_post_thumbnail() ) { ?>
 		<center>
              <div id="featured"> 
                 <?php the_post_thumbnail();
-             }
-           ?></div></center>
-                          <hr class="thin">
-                          <div class="row">
+             }?>
+                          <div class="row"><br>
                             <div class="col-md-8 col-sm-7">
-                              <h4 class="card-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php 
+                              <h4 class="card-title" align="left"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php 
                                                           $content = get_the_title();
                          	                                 $content = strip_tags($content);
                                                           echo substr($content, 0, 14); ?>[..]
@@ -66,10 +52,12 @@
                                                                           </a>
                               </h4>
                             </div>
+                          
                             <div class="col-md-4 col-sm-5">
                               <h1 class="ribbon"><strong class="ribbon-contentenido"><?php the_time( 'M j y' ); ?></strong></h1>
                             </div>
                           </div>
+           </div></center>  
                           <hr class="normal">
                           <div class="card-text">
 			   <?php the_excerpt();
