4634e0f5fdeb0987a13050dff865726c32cec6d2
travis <travis@browser.html>
2017-06-06 23:57:45 -0400

    Adding some backend TinyMCE functionality. The headers not working before sadly still wont work now

--- functions.php
+++ functions.php
@@ -1,3 +1,10 @@
 <?php
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 500, 500 ); // 50 pixels wide by 50 pixels tall, resize mode
+
+// Connect the WordPress post editor to your custom stylesheet
+function my_theme_add_editor_styles() {
+  add_editor_style( 'custom-editor-style.css' );
+}
+
+add_action( 'admin_init', 'my_theme_add_editor_styles' );
